16ecfc6629aedc88491719ca0bbc715ed92d9140
Arun Negi <Arun-Negi989>
2026-01-01 10:55:38 +0530

    condition about admin dashboard nad login page remove header and footer into the dashboard

--- src/App.js
+++ src/App.js
@@ -1,33 +1,44 @@
 import './assets/css/App.css';
-import { Routes, Route } from 'react-router-dom';
-import {} from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import AuthLayout from './layouts/auth';
 import AdminLayout from './layouts/admin';
-import {
-  ChakraProvider,
-  // extendTheme
-} from '@chakra-ui/react';
-import initialTheme from './theme/theme'; //  { themeGreen }
+import { ChakraProvider } from '@chakra-ui/react';
+import initialTheme from './theme/theme';
 import { useState } from 'react';
 import { Homepage } from 'components/Homepage';
-// Chakra imports
+
+import Header from 'components/navigations/Header';
+import Footer from 'components/navigations/Footer';
 
 export default function Main() {
-  // eslint-disable-next-line
   const [currentTheme, setCurrentTheme] = useState(initialTheme);
+  const location = useLocation();
+
+  // 🔹 admin & auth check
+  const isAdmin = location.pathname.startsWith('/admin');
+  const isAuth = location.pathname.startsWith('/auth');
+
+  const showHeaderFooter = !isAdmin && !isAuth;
+
   return (
     <ChakraProvider theme={currentTheme}>
+      {showHeaderFooter && <Header />}
+
       <Routes>
         <Route path="auth/*" element={<AuthLayout />} />
         <Route
           path="admin/*"
           element={
-            <AdminLayout theme={currentTheme} setTheme={setCurrentTheme} />
+            <AdminLayout
+              theme={currentTheme}
+              setTheme={setCurrentTheme}
+            />
           }
         />
-        
-        <Route path="/" element={< Homepage />} />
+        <Route path="/" element={<Homepage />} />
       </Routes>
+
+      {showHeaderFooter && <Footer />}
     </ChakraProvider>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,15 +4,11 @@ import { BrowserRouter } from 'react-router-dom';
 import './assets/css/App.css';
 
 import App from './App';
-import Header from 'components/navigations/Header';
-import Footer from 'components/navigations/Footer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <BrowserRouter>
-  <Header/>
     <App />
-    <Footer/>
-  </BrowserRouter>,
+  </BrowserRouter>
 );
