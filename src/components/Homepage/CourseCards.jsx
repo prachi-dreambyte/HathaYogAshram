d201fca841172051bc1480d139f66a2cbe55b631
pantprachi58 <pantprachi58@gmail.com>
2026-02-20 16:22:33 +0530

    20dateChanges

--- src/components/Homepage/CourseCards.jsx
+++ src/components/Homepage/CourseCards.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React from "react";
 import styles from "../../assets/styles/Homepage/CourseCards.module.css";
 import img1 from "../../assets/images/images.jpg";
 import img2 from "../../assets/images/international.webp";
